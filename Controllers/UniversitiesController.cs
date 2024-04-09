68f34a52052fae2127a0578c5b9cb4f5878d597e
Samer Shahbaz <165824535+samershahbaz1@users.noreply.github.com>
2024-04-09 15:47:19 +0500

    new changes

--- Controllers/UniversitiesController.cs
+++ Controllers/UniversitiesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using WebApplication1.Model.DataModels;
 using WebApplication1.Model.ViewModels;
@@ -18,7 +19,7 @@ namespace WebApplication1.Controllers
         private readonly DataContext _context;
         private readonly HttpClient _httpClient;
 
-        public UniversitiesController(ILogger<UniversitiesController> logger, DataContext context, HttpClient httpClient, UniversityRepository universityRepository)
+        public UniversitiesController(ILogger<UniversitiesController> logger,  DataContext context, HttpClient httpClient, UniversityRepository universityRepository)
         {
             _logger = logger;
             _context = context;
@@ -35,6 +36,15 @@ namespace WebApplication1.Controllers
             var universities = await result.Content.ReadAsStringAsync();
             await _universityRepository.SaveOrUpdate(country, JsonConvert.DeserializeObject<University[]>(universities));
             return Ok(JsonConvert.DeserializeObject<University[]>(universities));
+        }
+
+
+        [HttpGet("{country}/{state_province}")]
+        public async Task<IActionResult> GetUniversitiesByCountryAndState(string country, string state_province)
+        {
+            var result = await _context.Universities.Where(x => x.Country.ToLower().Contains(country.ToLower()) && x.State_Province.ToLower().Contains(state_province.ToLower())).ToListAsync();
+            var resultCOunt = result.Count;
+            return Ok(result);
         }
     }
 }
